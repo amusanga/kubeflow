2e437d59f19ca31b8fdb27f300dc4db807ab1845
amusanga <a.cesaire07@gmail.com>
2022-01-11 14:16:21 +0200

    Added kubeflow files

---
+++ Serving/build.sh
@@ -0,0 +1,6 @@
+CONTAINER_NAME=ml-pipeline-serving
+DOCKER_ID=amusanga
+TAG_NAME=v1
+docker build -t ${CONTAINER_NAME} .
+docker tag ${CONTAINER_NAME} ${DOCKER_ID}/${CONTAINER_NAME}:${TAG_NAME}
+docker push ${DOCKER_ID}/${CONTAINER_NAME}:${TAG_NAME}
