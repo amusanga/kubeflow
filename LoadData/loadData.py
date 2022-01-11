44fe89e3d73ff6e8dc250572ed2bdf0c329bc5d7
amusanga <a.cesaire07@gmail.com>
2022-01-11 16:42:08 +0200

    Adding components code

--- LoadData/loadData.py
+++ LoadData/loadData.py
@@ -1,1 +1,5 @@
-print(" ********************Data Loading******************* ")
+import torch
+import torchvision
+
+trainset = torchvision.datasets.CIFAR10(root="./data", train=True, download=True)
+testset = torchvision.datasets.CIFAR10(root="./data", train=False, download=True)
